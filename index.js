d0467234b77bb3d2d21a74e241d4e4cd93f6af75
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-03-01 11:08:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ document.querySelector('.share-button').addEventListener('click', function() {
     if(navigator.share) {
         navigator.share({
             title: 'Adekoye Adewale',
-            text: 'I am a Software Engineer and WordPress Developer based in Lagos, Nigeria.',
-            url: 'https://adekoyeadewale.com'
+            text: 'I am a Software Engineer, WordPress Developer, and Digital Marketing Strategist based in Lagos, Nigeria.',
+            url: 'https://adekoye.netlify.app/'
 })
 .then(() => console.log('Share complete'))
 .error((error) => console.error('Could not share at this time', error))
